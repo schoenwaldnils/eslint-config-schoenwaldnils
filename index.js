67b51785b44d2e342c537d338475cd765cf31234
Nils Schönwald <nils@schoen.world>
2020-03-15 12:10:59 +0100

    move to typescript

--- index.js
+++ index.js
@@ -1,45 +1,26 @@
 module.exports = {
-  parser: 'babel-eslint',
   extends: [
-    'airbnb',
+    'airbnb-typescript',
     'plugin:prettier/recommended',
     'prettier/flowtype',
     'prettier/react',
     'prettier/standard',
   ],
-  plugins: [
-    'react',
-    'prettier',
-  ],
-  settings: {
-    react: {
-      'jsx-boolean-value': 'always',
-    }
-  },
+  plugins: ['react', 'prettier'],
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    }
+    project: './tsconfig.json',
   },
   rules: {
     'import/prefer-default-export': 0,
     'react/jsx-boolean-value': 0,
     'react/jsx-props-no-spreading': 0,
-    'react/no-danger': 0,
-    'strict': 0,
-    'no-console': [
-      'warn',
-      { allow: [
-        'warn',
-        'error'
-      ]}
-    ],
-    'prettier/prettier': 2
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'prettier/prettier': 2,
   },
   env: {
     node: true,
     es6: true,
     jest: true,
     browser: true,
-  }
-};
+  },
+}
