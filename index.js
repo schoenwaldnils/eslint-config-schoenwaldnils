ee533bf4a14a4a15fccf466e75ab69008f82b4d4
Nils Schönwald <nils@schoen.world>
2021-06-05 02:54:43 +0200

    edit rules

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 module.exports = {
   extends: ['cellular'],
+  rules: {
+    'react/jsx-curly-brace-presence': [
+      'warn',
+      {
+        props: 'never',
+        children: 'never',
+      },
+    ],
+    'react/react-in-jsx-scope': 0,
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: true,
+        html: true,
+      },
+    ],
+  },
 }
